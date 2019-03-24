8eeec7afcdc2915e0680497b8eb8065f5d5c2fff
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2019-03-24 18:34:15 +0530

    Update alphavowel.py

--- alphavowel.py
+++ alphavowel.py
@@ -2,6 +2,6 @@ n=input()
 if (n=='a') or (n=='e') or (n=='i') or (n=='o') or (n=='u') or (n=='A') or (n=='E') or (n=='I') or (n=='O') or(n=='U'):
     print("Vowel")
 else:
-    print("Invalid")    
+    print("Invalid")   
 
 
